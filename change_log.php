cabb0a8ed14a4ef46295bc63dd9e8d212d99e047
DigitalCommunity <ant1freezeca@gmail.com>
2014-10-08 11:37:05 +0400

    fix change_log

--- change_log.php
+++ change_log.php
@@ -1,4 +1,9 @@
 <pre style="margin-top:50px;">
-<strong>- v6.6.6 - hctn</strong>
-- Release
+<strong>v0.0.1 - hctn</strong>
+- Генерация плаежного куркода
+</pre>
+
+<pre style="margin-top:50px;">
+<strong>v0.0.2 - hctn</strong>
+- Готов шаринг в соцсети, расшаривание куркода BBCode & HTML-тэгом
 </pre>
